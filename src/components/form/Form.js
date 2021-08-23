ea472b9d00e91053252e51616fbd11a9071d8e94
VictorBalanovskiy <82959239+VictorBalanovskiy@users.noreply.github.com>
2021-08-23 19:32:49 +0700

    Доделал ДЗ2
    Доделал 3 и 4 пункт ДЗ 2, но не разобрался как рендерить через .map, выдает ошибку, что мап не является функцией и как получить доступ к comment из Form в MessageList, чтоб рендерить этот массив? Через пропсы?

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,49 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from 'react';
+import './Form.css'
 
 export const Form = () => {
-    const [text, setText] = useState(0);
-    const [comment, setComment] = useState([]);
 
     const myRef = React.createRef();
+    const myRefAuthor = React.createRef();
 
-    const handleChangeText = () => {
-        let currentCount = text;
-        currentCount++;
-        setText(currentCount);
-    }
+    const [comment, setComment] = useState([]);
 
-    const addComment = () => {
-        let commentValue = myRef.currentCount.value;
-        let comments = [...comment, commentValue];
+    let handleChange = (e) => {
+        e.preventDefault();
+        let list = {
+            text: myRef.current.value,
+            author: myRefAuthor.current.value,
+        }
+        let comments = [...comment, list];
         setComment(comments);
         myRef.current.value = "";
+        myRefAuthor.current.value = "";
     }
 
+    useEffect(() => {
+        console.log('Добавлено сообщение');
+    })
+
     return (
-        <>
-            <h1>State</h1>
-            <div>
-                <button onClick={handleChangeText}>Click</button>
+            <div className="myForm">
+                <input className="myInput" type="text" placeholder="Author" ref={myRefAuthor}></input>
+                <textarea className="myTextarea" ref={myRef} placeholder="Message"></textarea>
+                <button className="myButton" onClick={handleChange}>Send</button>
             </div>
-            <div>
-                {text % 2 === 0 ? 'even' : 'odd'}
-            </div>
-            <div>
-                {text}
-            </div>
-            <div>
-                <textarea ref={myRef}></textarea>
-            </div>
-            <div>
-                <button onClick={addComment}>Add comment</button>
-            </div>
-            <div>
-                <ul>
-                    
-                </ul>
-            </div>
-        </>
-    );
+ )
     
 };
